170fb41d609627f185d94fc207006ab4daab0efb
suchintannit <suchintan.mishra@gmail.com>
2019-01-02 20:28:25 +0530

    Update index.js

--- src/getForm/index.js
+++ src/getForm/index.js
@@ -6,7 +6,7 @@ const formHTML = `<!DOCTYPE html>
   </head>
   <body>
     <h1>Subscribe to our newsletter</h1>
-    <form action="/getdata" method="post">
+    <form action="submit" method="get">
         <label>Email
             <input name="email" type="text">
         </label>
